f0f504caeeb7afcc42adc64722cd5fce9affda23
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-17 16:19:43 +0800

    lesson 06 - splash screen body splash content

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_ui_app/constants.dart';
+import 'package:flutter_ecommerce_ui_app/size_config.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -14,8 +15,25 @@ class _BodyState extends State<Body> {
         children: [
           Expanded(
             flex: 3,
-            child: Container( // can be Column, Row, SizedBox
-              color: Colors.deepOrangeAccent,
+            child: Column(
+              children: [
+                Spacer(),
+                Text(
+                  "NINJA",
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(36),
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text("Welcome to our Ninja shop!"),
+                Spacer(flex: 2),
+                Image.asset(
+                  "assets/images/splash_1.png",
+                  height: getProportionateScreenHeight(265),
+                  width: getProportionateScreenWidth(235),
+                ),
+              ],
             ),
           ),
           Expanded(
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_ui_app/screens/splash/components/body.dart';
+import 'package:flutter_ecommerce_ui_app/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // call it on the starting screen
+    SizeConfig().init(context);
     return Scaffold(
       body: Body(),
     );
