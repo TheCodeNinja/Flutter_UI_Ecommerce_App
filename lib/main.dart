5b9bba1e438194029d92998df00d7c54554b3b8b
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-18 03:50:21 +0800

    lesson 25 - extract ThemeData for class theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_ui_app/constants.dart';
 import 'package:flutter_ecommerce_ui_app/routes.dart';
 import 'package:flutter_ecommerce_ui_app/screens/splash/splash_screen.dart';
+import 'package:flutter_ecommerce_ui_app/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,27 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Muli',
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          elevation: 0,
-          brightness: Brightness.light,
-          iconTheme: IconThemeData(color: Colors.black),
-          textTheme: TextTheme(
-            headline6: TextStyle(
-              color: Color(0Xff8B8B8B),
-              fontSize: 18,
-            ),
-          ),
-        ),
-        textTheme: TextTheme(
-          bodyText1: TextStyle(color: kTextColor),
-          bodyText2: TextStyle(color: kTextColor),
-        ),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: theme(),
       // home: SplashScreen(),
       routes: routes,
       initialRoute: SplashScreen.routeName,
