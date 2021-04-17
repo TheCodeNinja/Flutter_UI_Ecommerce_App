d0c39de4c00cbaa27be1c06ebcdb3fb448bf76b4
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-17 15:42:52 +0800

    lesson 05 - splash screen body layout

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_ui_app/constants.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -8,8 +9,23 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container( // can be Column, Row, SizedBox
+              color: Colors.deepOrangeAccent,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container( // can be Column, Row, SizedBox
+              color: Colors.amberAccent,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
