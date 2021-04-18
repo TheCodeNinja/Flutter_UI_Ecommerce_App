39a445ddf184d6cf6ce9d6ee5b38fa7ff857d843
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-18 12:29:37 +0800

    lesson 27 - create class SignInForm with first TextFormField

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -7,22 +7,49 @@ class Body extends StatelessWidget {
     return SafeArea( // exclude small space for top and bottom
       child: SizedBox(
         width: double.infinity, // let Column as wide as its parent SafeArea
-        child: Column(
-          children: [
-            Text(
-              "Welcome back",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: getProportionateScreenWidth(28),
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: Column(
+            children: [
+              Text(
+                "Welcome back",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              "Sign in with your email and password \nor continue with social media",
-              textAlign: TextAlign.center,
-            ),
-          ],
+              Text(
+                "Sign in with your email and password \nor continue with social media",
+                textAlign: TextAlign.center,
+              ),
+              SignInForm(),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class SignInForm extends StatefulWidget {
+  @override
+  _SignInFormState createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "Email",
+              hintText: "Enter your email",     
+            ),
+          ),
+        ],
       ),
     );
   }
