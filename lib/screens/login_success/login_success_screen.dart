d00e7bc667b34c987f74343a9058daf09fd77190
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-20 00:54:57 +0800

    lesson 54 - login success screen body

--- lib/screens/login_success/login_success_screen.dart
+++ lib/screens/login_success/login_success_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'components/body.dart';
+
 class LoginSuccessScreen extends StatelessWidget {
   static String routeName = "/login_success";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        leading: SizedBox(), // hide go back arrow
+        title: Text("Login Success"),
+      ),
+      body: Body(),
     );
   }
 }
