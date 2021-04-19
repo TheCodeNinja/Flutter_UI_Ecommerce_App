6386c519b5ae02b9de5ba7644ab219cbe5041358
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-19 20:14:04 +0800

    lesson 46 - wrap sign_in_screen body column with SingleChildScrollView

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -14,59 +14,61 @@ class Body extends StatelessWidget {
         width: double.infinity, // let Column as wide as its parent SafeArea
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: Column(
-            children: [
-              SizedBox(height: SizeConfig.screenHeight * 0.04),
-              Text(
-                "Welcome back",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: getProportionateScreenWidth(28),
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Text(
+                  "Welcome back",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                "Sign in with your email and password \nor continue with social media",
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: SizeConfig.screenHeight * 0.08),
-              SignInForm(),
-              SizedBox(height: SizeConfig.screenHeight * 0.08),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SocialCard(
-                    icon: "assets/icons/google-icon.svg",
-                    press: () {},
-                  ),
-                  SocialCard(
-                    icon: "assets/icons/facebook-2.svg",
-                    press: () {},
-                  ),
-                  SocialCard(
-                    icon: "assets/icons/twitter.svg",
-                    press: () {},
-                  ),
-                ],
-              ),
-              SizedBox(height: getProportionateScreenWidth(20)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(fontSize: getProportionateScreenWidth(16)),
-                  ),
-                  Text(
-                    "Sign Up",
-                    style: TextStyle(
-                      fontSize: getProportionateScreenWidth(16),
-                      color: kPrimaryColor,
+                Text(
+                  "Sign in with your email and password \nor continue with social media",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignInForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialCard(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () {},
                     ),
-                  ),
-                ],
-              )
-            ],
+                    SocialCard(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenWidth(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(fontSize: getProportionateScreenWidth(16)),
+                    ),
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: getProportionateScreenWidth(16),
+                        color: kPrimaryColor,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
