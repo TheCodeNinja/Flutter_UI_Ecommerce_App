7cc1776943941451461556d0b9d5878ae03dd312
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-21 20:32:15 +0800

    lesson 64 - set up focus nodes for text form fields of otp_form

--- lib/screens/otp/components/otp_form.dart
+++ lib/screens/otp/components/otp_form.dart
@@ -10,6 +10,39 @@ class OtpForm extends StatefulWidget {
 }
 
 class _OtpFormState extends State<OtpForm> {
+
+  // FocusNode is an object that can be used by a stateful widget to obtain 
+  // the keyboard focus and to handle keyboard events.
+  
+  // Define the focus nodes
+  FocusNode pin2FocusNode;
+  FocusNode pin3FocusNode;
+  FocusNode pin4FocusNode;
+
+@override
+  void initState() {
+    super.initState();
+    // Create the focus nodes
+    pin2FocusNode = FocusNode();
+    pin3FocusNode = FocusNode();
+    pin4FocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // Clean up the focus nodes
+    pin2FocusNode.dispose();
+    pin3FocusNode.dispose();
+    pin4FocusNode.dispose();
+  }
+
+  void nextField(String value, FocusNode focusNode) {
+    if (value.length == 1) {
+      focusNode.requestFocus();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -28,40 +61,60 @@ class _OtpFormState extends State<OtpForm> {
                   keyboardType: TextInputType.number,
                   textAlign: TextAlign.center,
                   decoration: otpInputDecoration,
-                  onChanged: (value) {},
+                  onChanged: (value) {
+                    // Set up value for focusing pin2FocusNode focus node
+                    nextField(value, pin2FocusNode);
+                  },
                 ),
               ),
               SizedBox(
                 width: getProportionateScreenWidth(60),
                 child: TextFormField(
+                  // Pass focus node to textfield
+                  focusNode: pin2FocusNode,
                   obscureText: true,
                   style: TextStyle(fontSize: 24),
                   keyboardType: TextInputType.number,
                   textAlign: TextAlign.center,
                   decoration: otpInputDecoration,
-                  onChanged: (value) => {},
+                  onChanged: (value) {
+                    // Set up value for focusing pin3FocusNode focus node
+                    nextField(value, pin3FocusNode);
+                  },
                 ),
               ),
               SizedBox(
                 width: getProportionateScreenWidth(60),
                 child: TextFormField(
+                  // Pass focus node to textfield
+                  focusNode: pin3FocusNode,
                   obscureText: true,
                   style: TextStyle(fontSize: 24),
                   keyboardType: TextInputType.number,
                   textAlign: TextAlign.center,
                   decoration: otpInputDecoration,
-                  onChanged: (value) => {},
+                  onChanged: (value) {
+                    // Set up value for focusing pin4FocusNode focus node
+                    nextField(value, pin4FocusNode);
+                  },
                 ),
               ),
               SizedBox(
                 width: getProportionateScreenWidth(60),
                 child: TextFormField(
+                  // Pass focus node to textfield
+                  focusNode: pin4FocusNode,
                   obscureText: true,
                   style: TextStyle(fontSize: 24),
                   keyboardType: TextInputType.number,
                   textAlign: TextAlign.center,
                   decoration: otpInputDecoration,
-                  onChanged: (value) {},
+                  onChanged: (value) {
+                    if (value.length == 1) {
+                      pin4FocusNode.unfocus();
+                      // Then you need to check is the code is correct or not
+                    }
+                  },
                 ),
               ),
             ],
