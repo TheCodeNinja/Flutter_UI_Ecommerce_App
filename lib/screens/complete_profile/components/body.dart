9bf942de3aa1fcc04870d2c9da849e6f73848093
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-21 13:57:00 +0800

    lesson 58 - build widgets for complete_profile_screen (except the form)

--- lib/screens/complete_profile/components/body.dart
+++ lib/screens/complete_profile/components/body.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_ui_app/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        
-      ],
+    return SafeArea(
+      child: SizedBox(
+        // make it as wide as its parent widget
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(20),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.03),
+                // == Title ==
+                Text(
+                  "Complete Profile",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                // == Subtitle ==
+                Text(
+                  "Complete your details or continue \nwith social media",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.06),
+                // == Profile Form ==
+                Text("Our Form Here"),
+                // CompleteProfileForm(),
+                SizedBox(height: getProportionateScreenHeight(30)),
+                // Terms and Conditons Text
+                Text(
+                  "By continuing your confirm that you agree \nwith our Terms and Conditions",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.caption,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
