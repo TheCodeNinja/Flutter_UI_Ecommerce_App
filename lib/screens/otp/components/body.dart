0e12986e2cf393e9275353c2f45f3f25cfb287dc
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-21 18:56:10 +0800

    lesson 61 - build widgets for otp_screen (except timer and otp_form)

--- lib/screens/otp/components/body.dart
+++ lib/screens/otp/components/body.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_ui_app/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenWidth(20),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // == Title ==
+              Text(
+                "OTP Verification",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              // == Subtitle ==
+              Text("We sent your code to +852 8600 ****"),
+
+              // == Timer with text ==
+              Text("Timer With Text Here"),
+              
+              // == otp_form ==
+              Text("Our Otp Form Here"),
+              
+              // == Resend otp text ==
+              GestureDetector(
+                onTap: () {
+                  // OTP code resend
+                },
+                child: Text(
+                  "Resend OTP Code",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
