831e663350fb8693b88697a8c91600ac897d9055
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-28 19:38:54 +0800

    lesson 86 - [details] add SingleChildScrollView

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -16,53 +16,55 @@ class Body extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Body Top Section
-        // ~~~~~~~~~~~~~~~~
-        
-        ProductImages(product: product),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // Body Top Section
+          // ~~~~~~~~~~~~~~~~
+          
+          ProductImages(product: product),
 
-        // Body Bottom Section
-        // ~~~~~~~~~~~~~~~~~~~
-        
-        TopRoundedContainer(
-          color: Colors.white,
-          child: Column(
-            children: [
-              // product description
-              ProductDescription(
-                product: product,
-                pressOnSeeMore: () {},
-              ),
-              // icon buttons and default button
-              TopRoundedContainer(
-                color: Color(0xFFF6F7F9),
-                child: Column(
-                  children: [
-                    ColorDots(product: product),
-                    TopRoundedContainer(
-                      color: Colors.white,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                          left: SizeConfig.screenWidth * 0.15,
-                          right: SizeConfig.screenWidth * 0.15,
-                          top: getProportionateScreenWidth(15),
-                          bottom: getProportionateScreenWidth(40),
-                        ),
-                        child: DefaultButton(
-                          text: "Add to Cart",
-                          press: () {},
+          // Body Bottom Section
+          // ~~~~~~~~~~~~~~~~~~~
+          
+          TopRoundedContainer(
+            color: Colors.white,
+            child: Column(
+              children: [
+                // product description
+                ProductDescription(
+                  product: product,
+                  pressOnSeeMore: () {},
+                ),
+                // icon buttons and default button
+                TopRoundedContainer(
+                  color: Color(0xFFF6F7F9),
+                  child: Column(
+                    children: [
+                      ColorDots(product: product),
+                      TopRoundedContainer(
+                        color: Colors.white,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: SizeConfig.screenWidth * 0.15,
+                            right: SizeConfig.screenWidth * 0.15,
+                            top: getProportionateScreenWidth(15),
+                            bottom: getProportionateScreenWidth(40),
+                          ),
+                          child: DefaultButton(
+                            text: "Add to Cart",
+                            press: () {},
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
