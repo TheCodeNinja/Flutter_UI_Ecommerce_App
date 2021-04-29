4276194b55636f3f7aeeaf235088fad87489b6ae
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-29 10:36:13 +0800

    lesson 88 - [cart] appbar

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_ui_app/models/Cart.dart';
 
 class CartScreen extends StatelessWidget {
   static String routeName = "/cart";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: buildAppBar(context),
     );
   }
+
+  AppBar buildAppBar(BuildContext context) {
+     return AppBar(
+       title: Column(
+         children: [
+           Text(
+             "Your Cart",
+             style: TextStyle(color: Colors.black),
+           ),
+           Text(
+             "${demoCarts.length} items",
+             style: Theme.of(context).textTheme.caption,
+           ),
+         ],
+       ),
+     );
+   }
 }
