f8d3ff1ffd7921769b58288dded3026b2cb53fc8
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-04-29 11:52:59 +0800

    lesson 90 - [cart] add padding

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_ui_app/models/Cart.dart';
+import 'package:flutter_ecommerce_ui_app/size_config.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../components/cart_item_card.dart';
 
@@ -11,14 +13,20 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      // how long
-      itemCount: demoCarts.length,
-      // build widget
-      // map each index item to component
-      itemBuilder: (BuildContext context, int index) {
-        return CartItemCard(cartItem: demoCarts[index]); 
-      },
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: ListView.builder(
+        // how long
+        itemCount: demoCarts.length,
+        // build widget
+        // map each index item to component
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: CartItemCard(cartItem: demoCarts[index]),
+          ); 
+        },
+      ),
     );
   }
 }
